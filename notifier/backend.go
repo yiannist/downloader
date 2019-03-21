85543561f5a2c87774844aad2913beca636d91e1
Agis Anastasopoulos <a@xz0.org>
2019-03-21 12:54:00 +0200

    fixup! Initial Kafka backend prototype

--- notifier/backend.go
+++ notifier/backend.go
@@ -5,18 +5,25 @@ import "github.com/skroutz/downloader/job"
 // Backend is the interface that wraps the basic Notify method.
 //
 // Backend implementations are responsible for notifying about a job
-// completion through some notification channel, denoted by job.CallbackDst.
-//
-// Users should call Setup() once at the beginning before any calls to Notify.
+// completion through some notification channel (eg. HTTP, Kafka).
 type Backend interface {
-	// ID returns a constant string used as an identifier  for the
-	// concrete backend implementation.
-	ID() string
-
 	// Start() initializes the backend. Start() must be called once, before
 	// any calls to Notify.
 	Start() error
 
-	// Notify notifies about a job completion.
+	// Notify notifies about a job completion. Depending on the underlying
+	// implementation, Notify might be an asynchronous operation so a nil
+	// error does NOT necessarily mean the notification was delivered.
+	//
+	// Implementations should peek job.CallbackDst to determine the
+	// destination of the notification.
 	Notify(*job.Job) error
+
+	// ID returns a constant string used as an identifier for the
+	// concrete backend implementation.
+	ID() string
+
+	// Errors returns a channel on which notification delivery errors are
+	// emmitted.
+	//Errors() <-chan error
 }
